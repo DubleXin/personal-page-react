7cc8a94c0c1f11876e3c7e0e51cc6393089235e2
DubleXin <progames720.2@gmail.com>
2025-10-25 18:08:43 +0200

    add floating bg
    add floating bgremove unnecessary logs
    improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.scss";
-import { NavBar } from "./components";
+import { NavBar, ParallaxBackground } from "./components";
 import { Header, Footer, About, Skills, Testimonial, Work } from "./containers";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <ParallaxBackground />
       <Header />
       <About />
       <Work />
       <Skills />
       <Testimonial />
+      <ParallaxBackground />
       <Footer />
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar/NavBar";
 export { default as NavigationTabs } from "./NavigationTabs/NavigationTabs";
 export { default as SocialMedia } from "./SocialMedia/SocialMedia";
+export { default as ParallaxBackground } from "./ParallaxBackground/ParallaxBackground";
